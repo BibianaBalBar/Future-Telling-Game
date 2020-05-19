91ffca9a2216ef274fe54fba06d0c8c2bde9b868
Bibiana Barbosa <balbuenabibi@gmail.com>
2020-05-19 20:10:08 +0100

    update js

--- script.js
+++ script.js
@@ -1,30 +1,22 @@
-function random_number(num) {
-	// New function called random_choice that takes one parameter, num (or a number)
-	// Get a random number between 0 and a passed-in number
-	var num = num || 4; // If no number passed in, default to 4
-	return Math.floor(Math.random() * num); // Round the answer down (floor) of a random number between 0 and 1 and multiply it by a number. Then return a value and exit the function.
+function random_number(num = 4) {
+	return Math.floor(Math.random() * num);
 }
 
 function mash_choice() {
-	// New function called mash_choice that doesn't take any parameters
-	// Since MASH is a special case, give it its own list
-	var mash = [ 'mansion', 'apartment', 'shack', 'house' ]; // The array of choices to pick from
-	var randomNum = random_number(4); // Use the above function to get a number between 0 and 4
-	return mash[randomNum]; // Return the list item the random number function just picked and exit the function
+	let mash = [ 'mansion', 'apartment', 'shack', 'house' ];
+	let randomNum = random_number(4);
+	return mash[randomNum];
 }
 
 function get_answer(category) {
-	// Get a random answer from the available answers in a given category
-	var choices = []; // A blank array to hold the user provided answer
-	var selector = 'input[name="' + category + '[]"]'; // Build a CSS selector for the blanks in our passed in category
-	var inputs = document.querySelectorAll(selector); // Get all of the inputs that match our selector
-	var answer;
+	let choices = [];
+	let selector = 'input[name="' + category + '[]"]'; // Build a CSS selector for the blanks in our passed in category
+	let inputs = document.querySelectorAll(selector);
+	let answer;
 
-	for (var i = 0; i < inputs.length; i++) {
-		// Begin a for loop that will run through the code. i++ = add one to the counter which is "i"
-		answer = inputs[i].value; // Get the input with the index value of the counter and get the value ie. if they typed in dog, you get back "dog"
+	for (let i = 0; i < inputs.length; i++) {
+		answer = inputs[i].value;
 		if (answer !== '') {
-			// If answer doesn't equal a blank... !== means doesn't equal
 			choices.push(answer); //...add it to the end of the list
 		}
 	}
@@ -32,19 +24,18 @@ function get_answer(category) {
 }
 
 function fill_in_answers(answers) {
-	// Find the spans that need filled
-	var home = document.querySelector('#home'); // This says make a new variable and find the HTML tag that has the ID of "home"
-	var profession = document.querySelector('#profession');
-	var partner = document.querySelector('#partner');
-	var location = document.querySelector('#location');
+	let home = document.querySelector('#home'); // This says make a new variable and find the HTML tag that has the ID of "home"
+	let profession = document.querySelector('#profession');
+	let partner = document.querySelector('#partner');
+	let location = document.querySelector('#location');
 
 	// Fill them with the provided answers
 	home.innerText = answers['mash'];
 	profession.innerText = answers['profession'];
 	partner.innerText = answers['partner'];
 	location.innerText = answers['location'];
-	home.innerHTML = answers.mash; // Change the content of the element in the HTML doc with the id "home" to the "mash" value in answers
-	profession.innerHTML = answers.profession; // Change the content of the element in the HTML doc with the id "career" to the "career" value in answers
+	home.innerHTML = answers.mash;
+	profession.innerHTML = answers.profession;
 	partner.innerHTML = answers.partner;
 	location.innerHTML = answers.location;
 }
@@ -54,19 +45,19 @@ function handle_submission(evt) {
 	evt.stopPropagation(); // Stop the form from reloading the page
 
 	// Build up our answers object
-	var answersObject = {
+	const answersObject = {
 		mash: mash_choice(),
 		profession: get_answer('profession'),
 		partner: get_answer('partner'),
 		location: get_answer('location')
 	};
-	// Fill in the answers
+
 	fill_in_answers(answersObject);
 
-	var answer_div = document.querySelector('#answers');
+	let answer_div = document.querySelector('#answers');
 	answer_div.classList.add('show');
 }
 
 // Find the form on the page and attach a handler for when it's submitted
-var form = document.querySelector('#mash');
+let form = document.querySelector('#mash');
 form.addEventListener('submit', handle_submission); // Anytime the form is submitted, we want to call the function handle_submission
